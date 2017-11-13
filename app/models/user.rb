a588c6a0cd3a999edf947e5992a67fd31806f699
Axel <axel.iglesias@wolox.com.ar>
2017-11-13 14:30:14 -0300

    Made requested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules.
+  include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
 end
