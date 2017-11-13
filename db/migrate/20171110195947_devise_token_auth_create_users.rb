8b40eb43035f59ce9c97c77ef9ec6c84446cc02a
Axel <axel.iglesias@wolox.com.ar>
2017-11-13 12:04:06 -0300

    Corrected migration issues

--- db/migrate/20171110195947_devise_token_auth_create_users.rb
+++ db/migrate/20171110195947_devise_token_auth_create_users.rb
@@ -22,7 +22,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
 end
