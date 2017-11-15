44c61224f1d571f5d99624b141105d468483147d
Axel <axel.iglesias@wolox.com.ar>
2017-11-15 15:25:55 -0300

    Finished book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -22,70 +22,17 @@ describe Book, type: :model do
 end
 
 describe Book, type: :model do
-  subject(:book) do
-    described_class.new(
-      title: title, author: author, genre: genre, publisher: publisher,
-      image: image, year: year
-    )
-  end
-
-  let(:title) { Faker::Book.title }
-  let(:author) { Faker::Book.author }
-  let(:genre) { Faker::Book.genre }
-  let(:publisher) { Faker::Book.publisher }
-  let(:image) { 'image.jpg' }
-  let(:year) { '1990' }
-
   attributes = %i[title author genre publisher image year]
   attributes.each do |att|
-    context "When the #{att} is nil" do
-      let(att) { nil }
+    subject(:book) { build(:book) }
 
+    context "When the #{att} is nil" do
       it 'is invalid' do
+        book.public_send("#{att}=", nil)
         is_expected.to be_invalid
       end
     end
   end
-  #
-  # context 'When the author is nil' do
-  #   let(:author) { nil }
-  #
-  #   it 'is invalid' do
-  #     is_expected.to be_invalid
-  #   end
-  # end
-  #
-  # context 'When the genre is nil' do
-  #   let(:genre) { nil }
-  #
-  #   it 'is invalid' do
-  #     is_expected.to be_invalid
-  #   end
-  # end
-  #
-  # context 'When the publisher is nil' do
-  #   let(:publisher) { nil }
-  #
-  #   it 'is invalid' do
-  #     is_expected.to be_invalid
-  #   end
-  # end
-  #
-  # context 'When the image is nil' do
-  #   let(:image) { nil }
-  #
-  #   it 'is invalid' do
-  #     is_expected.to be_invalid
-  #   end
-  # end
-  #
-  # context 'When the year is nil' do
-  #   let(:year) { nil }
-  #
-  #   it 'is invalid' do
-  #     is_expected.to be_invalid
-  #   end
-  # end
 
   context 'When all attributes are valid' do
     it 'is valid' do
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :book do
     author  Faker::Book.author
     title   Faker::Book.title
-    year    '1990'
+    year    Faker::Number.between(1700, 2017)
     publisher Faker::Book.publisher
     genre   Faker::Book.genre
-    image   'image.jpg'
+    image   Faker::Avatar.image
   end
 end
