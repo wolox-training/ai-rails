6d2a48787ed3ece90b331a37c78cac21254806b8
Axel <axel.iglesias@wolox.com.ar>
2017-11-15 12:12:35 -0300

    Created book controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   root 'landing#index'
+  resources :books
+  get 'book/show', to:'book#show'
+  get 'book/index', to: 'book#index'
 end
