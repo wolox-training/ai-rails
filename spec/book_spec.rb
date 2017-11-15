ee7f73c5cc380c0f3a331c75da34c67b750cec42
Axel <axel.iglesias@wolox.com.ar>
2017-11-15 10:18:35 -0300

    corrected style issues

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -19,7 +19,9 @@ describe Book, type: :model do
   it do
     should validate_presence_of(:year)
   end
+end
 
+describe Book, type: :model do
   subject(:book) do
     described_class.new(
       title: title, author: author, genre: genre, publisher: publisher,
@@ -27,65 +29,67 @@ describe Book, type: :model do
     )
   end
 
-  let(:title)            { Faker::Book.title }
-  let(:author)             { Faker::Book.author }
-  let(:genre)                 { Faker::Book.genre }
-  let(:publisher)              { Faker::Book.publisher }
+  let(:title) { Faker::Book.title }
+  let(:author) { Faker::Book.author }
+  let(:genre) { Faker::Book.genre }
+  let(:publisher) { Faker::Book.publisher }
   let(:image) { 'image.jpg' }
   let(:year) { '1990' }
 
-  context 'When the title is nil' do
-    let(:title) { nil }
+  attributes = %i[title author genre publisher image year]
+  attributes.each do |att|
+    context "When the #{att} is nil" do
+      let(att) { nil }
 
-    it 'is invalid' do
-      is_expected.to be_invalid
+      it 'is invalid' do
+        is_expected.to be_invalid
+      end
     end
   end
-
-  context 'When the author is nil' do
-    let(:author) { nil }
-
-    it 'is invalid' do
-      is_expected.to be_invalid
-    end
-  end
-
-  context 'When the genre is nil' do
-    let(:genre) { nil }
-
-    it 'is invalid' do
-      is_expected.to be_invalid
-    end
-  end
-
-  context 'When the publisher is nil' do
-    let(:publisher) { nil }
-
-    it 'is invalid' do
-      is_expected.to be_invalid
-    end
-  end
-
-  context 'When the image is nil' do
-    let(:image) { nil }
-
-    it 'is invalid' do
-      is_expected.to be_invalid
-    end
-  end
-
-  context 'When the year is nil' do
-    let(:year) { nil }
-
-    it 'is invalid' do
-      is_expected.to be_invalid
-    end
-  end
+  #
+  # context 'When the author is nil' do
+  #   let(:author) { nil }
+  #
+  #   it 'is invalid' do
+  #     is_expected.to be_invalid
+  #   end
+  # end
+  #
+  # context 'When the genre is nil' do
+  #   let(:genre) { nil }
+  #
+  #   it 'is invalid' do
+  #     is_expected.to be_invalid
+  #   end
+  # end
+  #
+  # context 'When the publisher is nil' do
+  #   let(:publisher) { nil }
+  #
+  #   it 'is invalid' do
+  #     is_expected.to be_invalid
+  #   end
+  # end
+  #
+  # context 'When the image is nil' do
+  #   let(:image) { nil }
+  #
+  #   it 'is invalid' do
+  #     is_expected.to be_invalid
+  #   end
+  # end
+  #
+  # context 'When the year is nil' do
+  #   let(:year) { nil }
+  #
+  #   it 'is invalid' do
+  #     is_expected.to be_invalid
+  #   end
+  # end
 
   context 'When all attributes are valid' do
     it 'is valid' do
       is_expected.to be_valid
     end
   end
-
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :book do
     author  Faker::Book.author
     title   Faker::Book.title
-    year    "1990"
+    year    '1990'
     publisher Faker::Book.publisher
     genre   Faker::Book.genre
-    image   "image.jpg"
+    image   'image.jpg'
   end
 end
