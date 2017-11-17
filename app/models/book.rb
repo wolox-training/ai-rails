c6bffc7550928cbfaef005b31e981c2a73961b76
Axel <axel.iglesias@wolox.com.ar>
2017-11-17 16:21:59 -0300

    Added description a filter function on index book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
-  validates :author, :genre, :image, :title, :publisher, :year, presence: true
+  validates :author, :genre, :image, :title, :publisher, :year, :description, presence: true
+
+  scope :title, ->(title) { where('title ilike?', "%#{title}%") }
+  scope :author, ->(author) { where('lower(author) like?', "%#{author.downcase}%") }
+  scope :description, lambda { |description|
+    where('lower(description) like?', "%#{description.downcase}%")
+  }
+  scope :genre, ->(genre) { where genre: genre }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115132754) do
+ActiveRecord::Schema.define(version: 20171117173659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171115132754) do
     t.string "year", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "rents", force: :cascade do |t|
