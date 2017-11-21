606325e44debfa5034db2fd12f45d5af5031718e
Axel <axel.iglesias@wolox.com.ar>
2017-11-21 16:54:40 -0300

    Correcte lint issues

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -3,11 +3,11 @@ class RentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if params[:user_id].present?
-      @rents = Rent.includes(:user, :book).where(user_id: params[:user_id])
-    else
-      @rents = Rent.includes(:user, :book).where(book_id: params[:book_id])
-    end
+    @rents = if params[:user_id].present?
+               Rent.includes(:user, :book).where(user_id: params[:user_id])
+             else
+               Rent.includes(:user, :book).where(book_id: params[:book_id])
+             end
     render_paginated @rents, each_serializer: RentSerializer
   end
 
