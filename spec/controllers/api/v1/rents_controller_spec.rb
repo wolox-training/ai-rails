2ef13fd7a04a5f1d414d0e2507b532e413c20e2a
Axel <axel.iglesias@wolox.com.ar>
2017-11-22 12:38:46 -0300

    Deleted some commented lines

--- spec/controllers/api/v1/rents_controller_spec.rb
+++ spec/controllers/api/v1/rents_controller_spec.rb
@@ -69,24 +69,3 @@ describe RentsController, type: :controller do
     end
   end
 end
-
-#   describe 'GET #show' do
-#     context 'When fetching a book' do
-#       let!(:book) { create(:book) }
-#
-#       before do
-#         get :show, params: { id: book.id }
-#       end
-#
-#       it 'responses with the book json' do
-#         expect(response.body).to eq BookSerializer.new(
-#           book, root: false
-#         ).to_json
-#       end
-#
-#       it 'responds with 200 status' do
-#         expect(response).to have_http_status(:ok)
-#       end
-#     end
-#   end
-# end
