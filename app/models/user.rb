cc654c75264f493581429412be235d254badc49d
Axel <axel.iglesias@wolox.com.ar>
2017-11-24 16:00:29 -0300

    Corrected user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :rents, dependent: :destroy
-  has_many :booksuggestions, dependent: :destroy
+  has_many :book_suggestions, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
